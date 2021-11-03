83204530e3b1cf1984a233d896cc001e24887889
Ekaterina Miheeva <karamilllka@gmail.com>
2021-11-03 11:44:10 +0600

    addtest

---
+++ src/test/java/ru/netology/ProductRepositoryTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+    Product first = new Product(11231, "first", 500);
+    Product second = new Product(11324, "second", 2300);
+    Product third = new Product(21321, "third", 3200);
+
+    @Test
+   public void removeByIdNotFond() {
+        ProductRepository repo = new ProductRepository();
+        repo.save(first);
+        repo.save(second);
+        repo.save(third);
+
+        repo.removeById(11111);
+    }
+    @Test
+    public void removeById() {
+        ProductRepository repo = new ProductRepository();
+        repo.save(first);
+        repo.save(second);
+        repo.save(third);
+
+        repo.removeById(11231);
+    }
+}
