2ccafc45f7420bf1c5e184fe64777a4ee15ce472
Ekaterina Miheeva <karamilllka@gmail.com>
2021-11-03 11:46:23 +0600

    addtest

--- src/test/java/ru/netology/ProductRepositoryTest.java
+++ src/test/java/ru/netology/ProductRepositoryTest.java
@@ -2,15 +2,13 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ProductRepositoryTest {
     Product first = new Product(11231, "first", 500);
     Product second = new Product(11324, "second", 2300);
     Product third = new Product(21321, "third", 3200);
 
     @Test
-   public void removeByIdNotFond() {
+    public void removeByIdNotFond() {
         ProductRepository repo = new ProductRepository();
         repo.save(first);
         repo.save(second);
@@ -18,6 +16,7 @@ class ProductRepositoryTest {
 
         repo.removeById(11111);
     }
+
     @Test
     public void removeById() {
         ProductRepository repo = new ProductRepository();
